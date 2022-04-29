e740800e101555ef438e9030d7301c67f86b4620
Ahmed Oudouh <oudouhahmed@gmail.com>
2022-04-29 16:46:30 +0000

    i fixed error

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -9,10 +9,10 @@ import { Button } from "./button";
 export const Footer = () => {
   return (
     <div className="footer">
-      <Button name={"Trending"}icon={<MdLocalFireDepartment fontSize={20} />}/>
-      <Button name={"Movies"} icon={<MdOutlineMovie fontSize={20} />} />
-      <Button name={"TV Series"} icon={<CgScreen fontSize={20} />} />
-      <Button name={"FiSearch"} icon={<FiSearch fontSize={20} />} />
+      <Button name="Trending" icon={<MdLocalFireDepartment fontSize={20} />}/>
+      <Button name="Movies" icon={<MdOutlineMovie fontSize={20} />} />
+      <Button name="TV Series" icon={<CgScreen fontSize={20} />} />
+      <Button name="FiSearch" icon={<FiSearch fontSize={20} />} />
     </div>
   );
 };
