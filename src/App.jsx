64f2f719d08a2bb8b2217d5c0269bec6f5a70b00
youness <younessoudouh12@gmail.com>
2022-04-29 05:09:06 +0400

    Added trending page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Header from "./Componnets/header/Header";
+import { Routes, Route } from "react-router-dom";
+import Trending from "./pages/trending/Trending";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
+      <Routes>
+        <Route path="/" element={<Trending />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/trending/Trending.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Trending.css";
+
+const Trending = () => {
+  return (
+    <div className="trending">
+      <h1>Trending Today</h1>
+    </div>
+  );
+};
+
+export default Trending;
