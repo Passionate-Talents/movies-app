93bbfa00462ea663ca899434d03dfb27828cb467
Ahmed Oudouh <oudouhahmed@gmail.com>
2022-05-08 15:40:40 +0100

    fetching test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Footer } from "./footer/footer";
+import { Trending } from "./trending";
 
 function App() {
   return (
     <div className="App">
       <Footer/>
+      <Trending/>
     </div>
   );
 }
---
+++ src/singleContinent/singleContinent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const SingleContinent = () => {
+  return <div>
+      <img />
+      <p></p>
+      <span>
+          <span></span>
+      </span>
+  </div>;
+};
