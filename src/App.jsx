fb47507f3d8cda7c2b18ea7e03673a2e3d6e4658
youness <74568528+younessoudouh@users.noreply.github.com>
2022-05-27 14:23:42 +0400

    Revert "pagination"

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import { Header } from "./Componnets/header/Header";
-import { Trending } from "./pages/trending/Trending";
-import { Footer } from "./footer/footer";
+import { Trending } from "./pages/trending/trending";
+import { Footer } from "./Componnets/footer/footer";
+import { Movies } from "./pages/trending/movies/movies";
 
 function App() {
   return (
--- src/Componnets/movie-card/MovieCard.jsx
+++ src/Componnets/movie-card/MovieCard.jsx
@@ -1,30 +1,23 @@
 import React from "react";
-
-import "./MovieCard.css";
 import { Poster } from "../poster/Poster";
+import "./MovieCard.css";
 
 export const MovieCard = ({
   name,
   posterPath,
   title,
-  voteAverage,
-  mediaType,
-  releaseDate,
+  vote_average,
+  media_type,
+  release_date,
 }) => {
   return (
     <div className="movie-card">
-      {!!voteAverage && (
-        <span
-          className={`rate ${voteAverage < 6 ? "low-rating" : "high-rating"}`}
-        >
-          {voteAverage}
-        </span>
-      )}
+      <span className="rate">{vote_average}</span>
       <Poster name={name} src={posterPath} />
       <h2 className="movie-title">{title ?? name}</h2>
       <div className="movie-footer">
-        <h3 className="movie-type">{mediaType}</h3>
-        <h3 className="movie-date">{releaseDate ?? "2022 - 6 - 2"}</h3>
+        <h3 className="movie-type">{media_type}</h3>
+        <h3 className="movie-date">{release_date ?? "2022 - 6 - 2"}</h3>
       </div>
     </div>
   );
